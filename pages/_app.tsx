d24417ff55b57621d62825dcbb6ee6695acb4f40
bellasimi <gksmf6611@gmail.com>
2022-09-20 13:15:56 +0900

    feat: strapi 개발자 서버에 연동 및 app 초기화

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,8 +1,62 @@
-import '../styles/globals.css'
-import type { AppProps } from 'next/app'
+import { ApolloClient, ApolloProvider, createHttpLink, InMemoryCache } from '@apollo/client'
+import { CoProvider, AppShell } from '@co-design/core'
+import type { AppContext, AppProps } from 'next/app'
+import { Header } from '../components/Header'
+import { setContext } from '@apollo/client/link/context'
+import nookies from 'nookies'
 
+const httpLink = createHttpLink({
+  uri: 'http://localhost:1337/graphql',
+})
+
+const authLink = setContext((_, { headers }) => {
+  const { token } = nookies.get()
+  return {
+    headers: {
+      ...headers,
+      Authorization: token ? `Bearer ${token}` : '',
+    },
+  }
+})
+
+const client = new ApolloClient({
+  link: authLink.concat(httpLink),
+  cache: new InMemoryCache(),
+})
+
+/* export const getServerSideProps = async (ctx: AppProps) => {
+  const { token } = nookies.get(ctx)
+  return {
+    pageProps: {
+      token,
+    },
+  }
+}
+ */
 function MyApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  const header = (
+    <AppShell.Header height={70}>
+      <Header token={pageProps.token} />
+    </AppShell.Header>
+  )
+  return (
+    <ApolloProvider client={client}>
+      <CoProvider withNormalizeCSS withGlobalStyles>
+        <AppShell fixed header={header}>
+          <Component {...pageProps} />
+        </AppShell>
+      </CoProvider>
+    </ApolloProvider>
+  )
+}
+
+MyApp.getInitialProps = async (appCtx: AppContext) => {
+  const { token } = nookies.get(appCtx.ctx)
+  return {
+    pageProps: {
+      token,
+    },
+  }
 }
 
 export default MyApp
